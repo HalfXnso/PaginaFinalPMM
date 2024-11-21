1d80824bb073b5b57ae05a2364c9de8b84f1b464
HalfXnso <alfonsogaitanperez@gmail.com>
2024-11-21 12:41:04 +0100

    Terminado

--- app/src/main/java/com/example/paginafinalpmm/Greeting.kt
+++ app/src/main/java/com/example/paginafinalpmm/Greeting.kt
@@ -2,12 +2,16 @@ package com.example.paginafinalpmm
 
 import android.os.Bundle
 import android.widget.TextView
+import android.net.Uri
+import android.widget.VideoView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Greeting : AppCompatActivity() {
+    private lateinit var videoView: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,11 +24,18 @@ class Greeting : AppCompatActivity() {
 
         }
 
+        videoView = findViewById(R.id.videoView)
         val texto = findViewById<TextView>(R.id.textView4)
-        val usuario : String = intent.extras?.getString("USER_NAME").orEmpty();
+        val usuario: String = intent.extras?.getString("USER_NAME").orEmpty();
 
-
-        texto.text = "Bienvenido "+ usuario
+        val videoUri =
+            Uri.parse("android.resource://${packageName}/${R.raw.salchicha}")
+        videoView.setVideoURI(videoUri)
+        videoView.setOnPreparedListener { mp ->
+            mp.isLooping = true
+            videoView.start()
+        }
+        texto.text = "Bienvenido " + usuario
 
 
     }
